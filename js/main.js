0f0b6ccc6429883a6adf26c26ceb06e0b2385220
aaronbrown1989 <45995788+aaronbrown1989@users.noreply.github.com>
2019-03-06 10:27:44 +0000

    fix all problems

--- js/main.js
+++ js/main.js
@@ -7,3 +7,16 @@ $('.slick').slick({
 $("#my-menu").trigger("open");
 
 $("#my-menu").trigger("close");
+
+$(document).ready(function(){
+  $(".acceptcookies").on('click', function frontcookie(){
+      setCookie("acceptCookies", true, 365);
+    })
+});
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
